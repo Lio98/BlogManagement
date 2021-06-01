a7f5260f747f3d2f9e065b1a49f28dfe93c295b9
Lio <liutao19980127@gmail.com>
2021-06-02 07:55:07 +0800

    添加日志

---
+++ BlogManagement.Core/Log/LogFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace BlogManagement.Core
+{
+    public class LogFactory
+    {
+        public static Logger GetLogger()
+        {
+            string serilogOutputTemplate =
+                "{NewLine}{NewLine}Date：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}LogLevel：{Level}{NewLine}Message：{Message}{NewLine}{Exception}" +
+                new string('-', 50);
+
+            return new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File($"{AppContext.BaseDirectory}Logs\\log.log", LogEventLevel.Information,
+                    serilogOutputTemplate, rollingInterval: RollingInterval.Day).CreateLogger();
+        }
+
+    }
+}
